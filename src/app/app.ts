a2b4e1b1c3690f7a4cf5d4a8fc40899615bd29b3
Oscar Cedano <cedano@engineer.com>
2026-02-04 08:34:21 -0700

    feat: router link and datos-generales

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MenuComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
