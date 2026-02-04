d237378df2b23944b1bb0079a6d2d396561c907c
Jared Francisco Teran Teran <2023030482@upisn.edu.mx>
2026-02-04 08:50:20 -0700

    create: form

--- src/app/pages/calificaciones/calificaciones.component.ts
+++ src/app/pages/calificaciones/calificaciones.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-calificaciones',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './calificaciones.component.html',
   styleUrl: './calificaciones.component.css'
 })
 export class CalificacionesComponent {
-
+    
 }
